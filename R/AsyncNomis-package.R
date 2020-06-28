f7656bebf92a154bd9975492efb4f15d1bd71661
Konrad Zdeb <konrad.zdeb@me.com>
2020-06-28 14:58:48 +0100

    Build
    Minor dependnecy and documentation fixes.

---
+++ R/AsyncNomis-package.R
@@ -0,0 +1,11 @@
+#' @description
+#' Asynchronous querying of Nomis data in Shiny.
+#' @keywords internal
+#' @import promises
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
